303838ed9f428ce91e3d86e3539507ffc8f555fc
cristian Pedernera <enriquepedernera2022@gmail.com>
2022-09-13 23:13:23 -0300

    nuevos cambios

---
+++ index.js
@@ -0,0 +1,8 @@
+// adminitrador de mesas en un restaurante.
+for(let mesa = 1 ; mesa <=5; mesa++){
+    let nombre = prompt("ingrese su nombre");
+    let mensaje = `Mesa #${mesa} Nombre: ${nombre}`;
+    alert(mensaje);
+    
+}
+ 
